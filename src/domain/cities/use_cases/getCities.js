de1e5d9d4d9662fbea02bfdb9ccbf14c88c12061
Dcarrasc <David.carrascoo@usm.cl>
2024-07-02 06:04:25 -0400

    Desarrollo post control 3
    Se manejan los casos faltantes.

--- src/domain/cities/use_cases/getCities.js
+++ src/domain/cities/use_cases/getCities.js
@@ -7,24 +7,39 @@ exports.getAllCitiesUseCase = (ctx) => {
 
 exports.getCitiesByCountryUseCase = (ctx) => {
     
+    const country_search = ctx.params.country
     const cities = citiesRepository.searchCitiesByCountryName(ctx.params.country);
+    let contiene_numero = false
+    for (let letra of country_search){
+        if(!isNaN(letra)){
+            contiene_numero = true;
+            break;//Se termina el bucle al encontrar un numero
+        }
+    }
+        
+    if (country_search.length < 3){
+        ctx.status = 400;
+        ctx.body = { error: "El país especificado debe tener al menos 3 caracteres" };
+        return ctx;
+    }
     
-    
+    if (contiene_numero){
+        ctx.status = 400;
+        ctx.body = { message: 'Solo se aceptan caracteres no numéricos' }
+        return ctx;
+    }
+
     if (cities.length === 0) {
         ctx.status = 200;
         ctx.body = { message: "No se encontraron ciudades para el país ingresado" };
-    } 
-    
-    else if (cities.length < 3){
-        ctx.status = 400;
-        ctx.body = { message: "El país especificado debe tener al menos 3 caracteres" };
         return ctx;
     } 
+    
     else {
         ctx.body = cities;
     }
     return ctx
-    
+
 }
 
 exports.getCitiesByCityNameAndCountryUseCase = (ctx) => {
